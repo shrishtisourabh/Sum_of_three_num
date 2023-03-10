d0a66bfef38d12b007f29714d3b6dd9ab4e0cb2c
shrishtisourabh <shrishtisourabh@yahoo.com>
2023-03-10 14:29:17 +0530

    adding files

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("The sum of the given numbers is ");
+        int num1= 12;
+        int num2= 13;
+        int num3= 15;
+        int sum = num1+num2+num3;
+        System.out.println(sum);
+    }
+}
